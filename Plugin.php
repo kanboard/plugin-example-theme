329e7711a6b9afaf62fe362c8f5fe830ee33105e
Sebastian Tiede <info@sebastiantiede.de>
2015-10-13 21:15:09 +0200

    Fix namespace missing backslash / wrong class

--- Plugin.php
+++ Plugin.php
@@ -2,14 +2,14 @@
 
 namespace Plugin\Theme;
 
-use Core\PluginBase;
+use Core\Plugin\Base;
 
-class Plugin extends PluginBase
+class Plugin extends Base
 {
     public function initialize()
     {
         $this->template->hook->attach('template:layout:head', 'theme:layout/head');
         $this->template->hook->attach('template:layout:top', 'theme:layout/top');
-        $this->template->setTemplateOverride('template:header', 'theme:layout/header');
+        $this->template->setTemplateOverride('header', 'theme:layout/header');
     }
 }
